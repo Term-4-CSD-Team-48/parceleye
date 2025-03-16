89c269f1270a7135b9d90eb05337f5efe41bdff0
Jing Huan <124416920+jhjh0409@users.noreply.github.com>
2025-03-17 00:13:44 +0800

    Added Sign Up redirect function

--- app/src/main/java/com/example/parceleyelogin/MainActivity.java
+++ app/src/main/java/com/example/parceleyelogin/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.parceleyelogin;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView signUp;
+    String signUpText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +34,33 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        signUp = findViewById(R.id.signUp);
+
+        // Setting custom colour for "Sign Up"
+        signUpText = "Don't have an account? Sign Up";
+        SpannableString spannableString = new SpannableString(signUpText);
+        ForegroundColorSpan colorSpan = new ForegroundColorSpan(Color.parseColor("#FF000000"));
+        spannableString.setSpan(colorSpan, 23, signUpText.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        // Setting Sign Up to be clickable
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                Intent register = new Intent(MainActivity.this, Register.class);
+                startActivity(register);
+            }
+
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setColor(Color.parseColor("#3685cd")); // Keep custom color
+                ds.setUnderlineText(false); // Remove underline
+            }
+        };
+
+        spannableString.setSpan(clickableSpan, 23, signUpText.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        signUp.setText(spannableString);
+        signUp.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
