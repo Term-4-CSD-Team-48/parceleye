e6816f1309900ebf79f7b9b0004a998250f572c6
Jing Huan <124416920+jhjh0409@users.noreply.github.com>
2025-04-03 02:37:01 +0800

    Implemented logout function

--- app/src/main/java/com/example/parceleyelogin/ProfileFragment.java
+++ app/src/main/java/com/example/parceleyelogin/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.parceleyelogin;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,14 +8,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class ProfileFragment extends Fragment {
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        Button logout = view.findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent login = new Intent(getActivity(), MainActivity.class);
+                startActivity(login);
+            }
+        });
     }
 }
