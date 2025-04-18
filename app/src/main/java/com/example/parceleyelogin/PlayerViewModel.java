32e536d0be21204c2c82f38046515cb9cfe167f2
b6Ah_SHG-262-Wb <xyf.oco@gmail.com>
2025-04-18 20:21:46 +0800

    docs: documented observe and prompt

--- app/src/main/java/com/example/parceleyelogin/PlayerViewModel.java
+++ app/src/main/java/com/example/parceleyelogin/PlayerViewModel.java
@@ -1,5 +1,7 @@
 package com.example.parceleyelogin;
 
+import static com.google.android.gms.common.util.CollectionUtils.mapOf;
+
 import android.content.Context;
 import android.util.Log;
 
@@ -13,12 +15,22 @@ import androidx.media3.common.Timeline;
 import androidx.media3.common.util.UnstableApi;
 import androidx.media3.datasource.DataSource;
 import androidx.media3.datasource.DefaultHttpDataSource;
+import androidx.media3.datasource.okhttp.OkHttpDataSource;
 import androidx.media3.exoplayer.ExoPlayer;
 import androidx.media3.exoplayer.hls.DefaultHlsExtractorFactory;
 import androidx.media3.exoplayer.hls.HlsManifest;
 import androidx.media3.exoplayer.hls.HlsMediaSource;
 import androidx.media3.exoplayer.source.DefaultMediaSourceFactory;
 import androidx.media3.exoplayer.util.EventLogger;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import okhttp3.Cookie;
+import okhttp3.CookieJar;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
 
 public class PlayerViewModel extends ViewModel {
 
@@ -31,6 +43,8 @@ public class PlayerViewModel extends ViewModel {
             exoPlayer = new ExoPlayer.Builder(context).setMediaSourceFactory(
                     new DefaultMediaSourceFactory(context).setLiveTargetOffsetMs(1000)).build();
 
+            // Request to API to observe the AI.
+            // Release the player if unsuccessful
             ApiClient.observe(MyFirebaseMessagingService.getToken(context), new ApiClient.CallbackParts() {
                 @Override
                 public void onResponse(int code) {
@@ -47,8 +61,28 @@ public class PlayerViewModel extends ViewModel {
                 }
             });
 
+            // Make the player always send a JSESSIONID cookie with every request
+            OkHttpClient okHttpClient = new OkHttpClient.Builder().cookieJar(new CookieJar() {
+                @Override
+                public void saveFromResponse(@NonNull HttpUrl httpUrl, @NonNull List<Cookie> list) {}
+
+                @NonNull
+                @Override
+                public List<Cookie> loadForRequest(@NonNull HttpUrl httpUrl) {
+                    List<Cookie> cookieList = new ArrayList<>();
+                    cookieList.add(new Cookie.Builder()
+                            .name("JSESSIONID")
+                            .value(ApiClient.sessionId.get())
+                            .domain(httpUrl.host())
+                            .path("/")
+                            .httpOnly()
+                            .build());
+                    return cookieList;
+                }
+            }).build();
+
             // Create a data source factory.
-            DataSource.Factory dataSourceFactory = new DefaultHttpDataSource.Factory();
+            DataSource.Factory dataSourceFactory = new OkHttpDataSource.Factory(okHttpClient);
 
             // Create a HLS media source pointing to a playlist uri and set it to player
             HlsMediaSource hlsMediaSource =
@@ -57,7 +91,8 @@ public class PlayerViewModel extends ViewModel {
                             .setAllowChunklessPreparation(false)
                             //https://stream-akamai.castr.com/5b9352dbda7b8c769937e459/live_2361c920455111ea85db6911fe397b9e/index.fmp4.m3u8
                             //http://54.254.147.39:8080/hls/stream.m3u8
-                            .createMediaSource(MediaItem.fromUri("https://stream-akamai.castr.com/5b9352dbda7b8c769937e459/live_2361c920455111ea85db6911fe397b9e/index.fmp4.m3u8"));
+                            .createMediaSource(MediaItem.fromUri("http://54.179.124.182:8080/hls/stream.m3u8"));
+                            //.createMediaSource(MediaItem.fromUri("https://stream-akamai.castr.com/5b9352dbda7b8c769937e459/live_2361c920455111ea85db6911fe397b9e/index.fmp4.m3u8"));
             exoPlayer.setMediaSource(hlsMediaSource);
             exoPlayer.addAnalyticsListener(new EventLogger());
             exoPlayer.addListener(
